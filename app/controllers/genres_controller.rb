5cb83ee6a1afc26e52a7b6a116d4e6be4d8e208a
github username <github email address>
2019-03-05 12:17:44 -0500

    added genres add function

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -7,5 +7,28 @@ class GenresController < ApplicationController
     @genre = Genre.find(params[:id])
   end
 
+  def new
+    if current_user && current_user.admin
+      @genre = Genre.new
+    else
+      redirect_to movies_path
+    end
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to genres_path(@genre)
+    else
+      redirect_to new_genre_path
+    end
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
 
 end
